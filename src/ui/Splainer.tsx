f8ab55e2fdc6e8985d1edacbc15169429f843dd4
Johan Althoff <teetow@gmail.com>
2022-01-13 19:19:30 +0100

    style: make more mobile-friendly

--- src/ui/Splainer.tsx
+++ src/ui/Splainer.tsx
@@ -7,10 +7,10 @@ const Splainer = () => {
         Made with <a href="https://www.elementary.audio/">Elementary Audio</a>
       </span>
       <span>
-        Source on <a href="https://github.com/teetow/elementary_grid">GitHub</a>
+        <a href="https://github.com/teetow/elementary_grid">GitHub</a>
       </span>
       <span>
-        Check out my <a href="https://soundcloud.com/teetow">SoundCloud</a>
+        My <a href="https://soundcloud.com/teetow">SoundCloud</a>
         {" =)"}
       </span>
     </div>
