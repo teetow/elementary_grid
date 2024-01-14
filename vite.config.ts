601cc4f2c9ae85870684fa89ffd963d72e7957ae
Teetow <teetow@gmail.com>
2024-01-15 00:46:36 +0100

    chore: fix build

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,9 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "./",
+  build: {
+    assetsDir: "./",
+  },
   plugins: [react()],
 });
